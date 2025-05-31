f078bcee026a1b07128258ab2ff6a15965f382d1
Bruno Sastre <brfignoni@gmail.com>
2025-05-31 02:23:41 -0300

    Poner a `noreply@mvdwash.com.uy` como remitente
    Poner a `noreply@mvdwash.com.uy` como remitente para evitar que el correo vaya a spam

--- assets/email-templates/contact-form.php
+++ assets/email-templates/contact-form.php
@@ -1,5 +1,5 @@
 <?php
-if( ! empty( $_POST['email'] ) ) {
+if (! empty($_POST['email'])) {
 
 	// Enable / Disable SMTP
 	$enable_smtp = 'no'; // yes OR no
@@ -17,60 +17,59 @@ if( ! empty( $_POST['email'] ) ) {
 	$grecaptcha_secret_key = 'YOUR_SECRET_KEY';
 
 	$from 	= $_POST['email'];
-	$name 	= isset( $_POST['name'] ) ? $_POST['name'] : '';
+	$name 	= isset($_POST['name']) ? $_POST['name'] : '';
 
-	if( ! empty( $grecaptcha_secret_key ) && ! empty( $_POST['g-recaptcha-response'] ) ) {
+	if (! empty($grecaptcha_secret_key) && ! empty($_POST['g-recaptcha-response'])) {
 
 		$token = $_POST['g-recaptcha-response'];
 
 		// call curl to POST request
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+		curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query( array( 'secret' => $grecaptcha_secret_key, 'response' => $token ) ) );
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => $grecaptcha_secret_key, 'response' => $token)));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$response = curl_exec($ch);
 		curl_close($ch);
 		$arrResponse = json_decode($response, true);
 
 		// verify the response
-		if( isset( $_POST['action'] ) && ! ( isset( $arrResponse['success'] ) && $arrResponse['success'] == '1' && $arrResponse['action'] == $_POST['action'] && $arrResponse['score'] = 0.5 ) ) {
+		if (isset($_POST['action']) && ! (isset($arrResponse['success']) && $arrResponse['success'] == '1' && $arrResponse['action'] == $_POST['action'] && $arrResponse['score'] = 0.5)) {
 
 			echo '{ "alert": "alert-danger", "message": "Tu mensaje no pudo ser enviado debido a un reCaptcha inválido!" }';
 			die;
-
-		} else if( ! isset( $_POST['action'] ) && ! ( isset( $arrResponse['success'] ) && $arrResponse['success'] == '1' ) ) {
+		} else if (! isset($_POST['action']) && ! (isset($arrResponse['success']) && $arrResponse['success'] == '1')) {
 
 			echo '{ "alert": "alert-danger", "message": "Tu mensaje no pudo ser enviado debido a un reCaptcha inválido!" }';
 			die;
 		}
 	}
 
-	if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-		$prefix		= !empty( $_POST['prefix'] ) ? $_POST['prefix'] : '';
+		$prefix		= !empty($_POST['prefix']) ? $_POST['prefix'] : '';
 		$submits	= $_POST;
 		$botpassed	= false;
 
 		$fields = array();
-		foreach( $submits as $name => $value ) {
-			if( empty( $value ) ) {
+		foreach ($submits as $name => $value) {
+			if (empty($value)) {
 				continue;
 			}
 
-			$name = str_replace( $prefix , '', $name );
-			$name = function_exists('mb_convert_case') ? mb_convert_case( $name, MB_CASE_TITLE, "UTF-8" ) : ucwords($name);
+			$name = str_replace($prefix, '', $name);
+			$name = function_exists('mb_convert_case') ? mb_convert_case($name, MB_CASE_TITLE, "UTF-8") : ucwords($name);
 
-			if( is_array( $value ) ) {
-				$value = implode( ', ', $value );
+			if (is_array($value)) {
+				$value = implode(', ', $value);
 			}
 
-			$fields[$name] = nl2br( filter_var( $value, FILTER_SANITIZE_SPECIAL_CHARS ) );
+			$fields[$name] = nl2br(filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS));
 		}
 
 		$response = array();
-		foreach( $fields as $fieldname => $fieldvalue ) {
-			if( $template == 'text' ) {
+		foreach ($fields as $fieldname => $fieldvalue) {
+			if ($template == 'text') {
 				$response[] = $fieldname . ': ' . $fieldvalue;
 			} else {
 				$fieldname = '<tr>
@@ -94,33 +93,33 @@ if( ! empty( $_POST['email'] ) ) {
 				<td width="50%" align="right">&nbsp;</td>
 				<td align="left">&nbsp;</td>
 				</tr>
-				' . implode( '', $response ) . '
+				' . implode('', $response) . '
 				</table>
 			</body>
 			</html>';
-		if( $enable_smtp == 'no' ) { // Simple Email
+		if ($enable_smtp == 'no') { // Simple Email
 
 			// Always set content-type when sending HTML email
 			$headers = "MIME-Version: 1.0" . "\r\n";
 			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 			// More headers
-			$headers .= 'From: ' . $fields['Name'] . ' <' . $fields['Email'] . '>' . "\r\n";
-			if( mail( $receiver_email, $subject, $message, $headers ) ) {
+			$headers .= 'From: Mvd Wash <noreply@mvdwash.com.uy>' . "\r\n";
+			$headers .= 'Reply-To: ' . $fields['Email'] . "\r\n";
+			if (mail($receiver_email, $subject, $message, $headers)) {
 
 				// Redirect to success page
-				$redirect_page_url = ! empty( $_POST['redirect'] ) ? $_POST['redirect'] : '';
-				if( ! empty( $redirect_page_url ) ) {
-					header( "Location: " . $redirect_page_url );
+				$redirect_page_url = ! empty($_POST['redirect']) ? $_POST['redirect'] : '';
+				if (! empty($redirect_page_url)) {
+					header("Location: " . $redirect_page_url);
 					exit();
 				}
 
-			   	//Success Message
-			  	echo '{ "alert": "alert alert-success alert-dismissable", "message": "Gracias, responderemos a la brevedad!" }';
+				//Success Message
+				echo '{ "alert": "alert alert-success alert-dismissable", "message": "Gracias, responderemos a la brevedad!" }';
 			} else {
 				//Fail Message
-			  	echo '{ "alert": "alert alert-danger alert-dismissable", "message": "Tu mensaje no pudo ser enviado!" }';
+				echo '{ "alert": "alert alert-danger alert-dismissable", "message": "Tu mensaje no pudo ser enviado!" }';
 			}
-			
 		} else { // SMTP
 			// Email Receiver Addresses
 			$toemailaddresses = array();
@@ -142,31 +141,32 @@ if( ! empty( $_POST['email'] ) ) {
 			$mail->Password = 'YOUR_SMTP_PASSWORD'; // Your Password
 			$mail->SMTPSecure = 'ssl'; // Your Secure Connection
 			$mail->Port     = 465; // Your Port
-			$mail->setFrom( $fields['Email'], $fields['Name'] );
-			
-			foreach( $toemailaddresses as $toemailaddress ) {
-				$mail->AddAddress( $toemailaddress['email'], $toemailaddress['name'] );
+			$mail->setFrom('noreply@mvdwash.com.uy', 'Mvd Wash');
+			$mail->addReplyTo($fields['Email'], $fields['Name']);
+
+			foreach ($toemailaddresses as $toemailaddress) {
+				$mail->AddAddress($toemailaddress['email'], $toemailaddress['name']);
 			}
 
 			$mail->Subject = $subject;
-			$mail->isHTML( true );
+			$mail->isHTML(true);
 
 			$mail->Body = $message;
 
-			if( $mail->send() ) {
-				
+			if ($mail->send()) {
+
 				// Redirect to success page
-				$redirect_page_url = ! empty( $_POST['redirect'] ) ? $_POST['redirect'] : '';
-				if( ! empty( $redirect_page_url ) ) {
-					header( "Location: " . $redirect_page_url );
+				$redirect_page_url = ! empty($_POST['redirect']) ? $_POST['redirect'] : '';
+				if (! empty($redirect_page_url)) {
+					header("Location: " . $redirect_page_url);
 					exit();
 				}
 
-			   	//Success Message
-			  	echo '{ "alert": "alert alert-success alert-dismissable", "message": "Gracias, responderemos a la brevedad!" }';
+				//Success Message
+				echo '{ "alert": "alert alert-success alert-dismissable", "message": "Gracias, responderemos a la brevedad!" }';
 			} else {
 				//Fail Message
-			  	echo '{ "alert": "alert alert-danger alert-dismissable", "message": "Tu mensaje no pudo ser enviado!" }';
+				echo '{ "alert": "alert alert-danger alert-dismissable", "message": "Tu mensaje no pudo ser enviado!" }';
 			}
 		}
 	}
